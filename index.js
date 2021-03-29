86ef08a5ee3eb38f9d6e22412b9f121f6f9e2f7c
Stenio Dias Rapchan <steniodr@hotmail.com>
2021-03-29 13:14:35 -0300

    add POST Request

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded ({ extended: false }))
+app.use(express.json())
 
 var title;
 app.get('/', (req, res) => {
@@ -13,6 +15,19 @@ app.get('/', (req, res) => {
 app.get('/perguntar', (req, res) =>{
     title = 'Faça sua pergunta';
     res.render('perguntar', {title: title});
+})
+
+app.post('/salvar-pergunta', (req, res) =>{
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    let autor;
+    if (req.body.autor) autor = req.body.autor;
+    else autor = 'Anônimo';
+
+    res.send(`Formulário recebido!
+    Titulo: ${titulo}.
+    Descrição: ${descricao}.
+    Autor: ${autor}.`)
 })
 
 app.listen(8181, ()=>{console.log('App rodando na porta 8181.');})
