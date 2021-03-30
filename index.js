3e30fca6d9687dea648a3b34f3412502c6f2a830
Stenio Dias Rapchan <steniodr@hotmail.com>
2021-03-30 10:54:22 -0300

    update routes in index.js and add dynamic response pages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 
 //Configurações de uso
 app.set('view engine', 'ejs');
@@ -17,6 +18,7 @@ connection.authenticate()
     .catch((err) => {
         console.log('Erro encontrado: ' + err);
     })
+
 //Rotas
 var title;
 app.get('/', (req, res) => {
@@ -27,6 +29,37 @@ app.get('/', (req, res) => {
 app.get('/perguntar', (req, res) =>{
     title = 'Faça sua pergunta';
     res.render('perguntar', {title: title});
+})
+
+app.get('/todas-perguntas', (re, res)=>{
+    title = 'Todas as perguntas';
+
+    Pergunta.findAll({ raw: true, order:[['id', 'DESC']]}).then(perguntas =>{
+        res.render('todas-perguntas', {title: title, perguntas: perguntas})
+    })
+})
+
+app.get('/pergunta/:id', (req, res) => {
+
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then((pergunta)=>{
+        if(pergunta){
+            title = pergunta.titulo;
+            Resposta.findAll({
+                where: {perguntaID: pergunta.id},
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render('pergunta', {title: title, pergunta: pergunta, respostas: respostas});
+            })
+
+        } else{
+            title = 'Erro encontrado';
+            res.render('errors', {title: title});
+        }
+    })
+
 })
 
 app.post('/salvar-pergunta', (req, res) =>{
@@ -42,7 +75,24 @@ app.post('/salvar-pergunta', (req, res) =>{
         autor: autor
     }).then(() => {
         title = 'Pergunta salva com sucesso!'
-        res.render('salvar-pergunta', {title: title})
+        res.render('success-response', {title: title})
+    })
+})
+
+app.post('/salvar-resposta', (req, res) =>{
+    let corpo = req.body.corpo;
+    let perguntaID = req.body.perguntaID;
+    let autor_resposta;
+    if (req.body.autor_resposta) autor_resposta = req.body.autor_resposta;
+    else autor_resposta = 'Anônimo';
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaID: perguntaID,
+        autor_resposta: autor_resposta
+    }).then(() =>{
+        title = 'Resposta salva com sucesso!'
+        res.render('success-response', {title: title})
     })
 })
 
